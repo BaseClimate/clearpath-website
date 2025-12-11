fb375de0d70fbbdc2347fd102bc2b517a0fdd47a
BaseClimate <jon@baseclimate.com>
2025-12-10 16:24:11 -0800

    Update contact phone numbers in config

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@ export const SITE = {
   DOMAIN: 'clearpath.direct',
   BASE_URL: 'https://www.clearpath.direct/',
   EMAIL: 'info@clearpath.direct',
-  PHONE_DISPLAY: '(289) 217-2448',
-  PHONE_TEL: '+12892172448',
+  PHONE_DISPLAY: '(289) 367-6338',
+  PHONE_TEL: '+12893676338',
 };
